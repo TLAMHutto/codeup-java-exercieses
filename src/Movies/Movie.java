cdf9cb278945154fe379504c405b594097c6d743
Keaton Hutto <keatonhutto8@gmail.com>
2022-05-22 23:53:10 -0500

    movie

---
+++ src/Movies/Movie.java
@@ -0,0 +1,11 @@
+package Movies;
+
+public class Movies {
+    private String title;
+    private String category;
+    public void movies(String title, String category) {
+        this.title = title;
+        this.category = category;
+    }
+
+}
