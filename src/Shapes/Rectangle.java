871a73346c7ae3f14d521bb5f1a2c4e8a9722887
Keaton Hutto <keatonhutto8@gmail.com>
2022-05-25 16:23:00 -0500

    interfaces

--- src/Shapes/Rectangle.java
+++ src/Shapes/Rectangle.java
@@ -1,16 +1,21 @@
 package Shapes;
 
-public class Rectangle {
-    protected double width;
-    protected double height;
+//public class Rectangle {
+//    protected double width;
+//    protected double height;
+//    public Rectangle(double width, double height) {
+//        this.width = width;
+//        this.height = height;
+//    }
+//    public double perimeter() {
+//        return 2 * (width + height);
+//    }
+//    public double getArea() {
+//        return width * height;
+//    }
+//}
+public class Rectangle extends Quadrilateral implements Measurable {
     public Rectangle(double width, double height) {
-        this.width = width;
-        this.height = height;
-    }
-    public double perimeter() {
-        return 2 * (width + height);
-    }
-    public double getArea() {
-        return width * height;
+        super(width, height, width, height);
     }
 }
